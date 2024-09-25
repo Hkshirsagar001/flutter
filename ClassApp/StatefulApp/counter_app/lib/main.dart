570b233c9ca638416bfa09bba4130c19ee7b47b4
Harshkshirsagar0 <harshkshirsagar2003@gmail.com>
2024-09-26 00:09:16 +0530

    counter app stateful widgets

---
+++ ClassApp/StatefulApp/counter_app/lib/main.dart
@@ -0,0 +1,43 @@
+import "package:flutter/material.dart";
+ 
+
+void main(){ 
+  runApp( const CounterApp());
+}
+
+class CounterApp extends StatefulWidget{  
+  const CounterApp({super.key});
+
+  @override
+  State createState() => _CounterAppState();
+}
+
+class _CounterAppState extends State{ 
+
+  int count = 0; 
+
+  @override
+  Widget build(BuildContext context){  
+    return  MaterialApp( 
+      home:Scaffold( 
+        appBar: AppBar(   
+          title: const Text("COUNTER APP"),
+          centerTitle:true , 
+          backgroundColor: Colors.blue[800],  
+        ),
+        body: Center(  
+          child: Text("$count"),
+        ),
+        floatingActionButton:FloatingActionButton(  
+          onPressed:(){  
+            count++; 
+
+            setState(() {});
+          },
+          child: const Icon(Icons.add),
+
+        ),
+      )
+    );
+  }
+}
